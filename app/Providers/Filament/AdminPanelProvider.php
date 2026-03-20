58ec7aa2c21ae72c4d3912f8a0078c87515a8836
Leandro <lean.ayala4@gmail.com>
2026-03-20 12:33:17 -0300

    Eliminar Footer

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,8 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\View\PanelsRenderHook;
+use Illuminate\Support\Facades\Blade;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -24,7 +26,6 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->footer(false)
             ->id('admin')
             ->path('admin')
             ->login()
@@ -40,6 +41,10 @@ class AdminPanelProvider extends PanelProvider
                 'Pacientes',
                 'Configuración',
             ])
+            ->renderHook(
+                PanelsRenderHook::FOOTER,
+                fn () => ''
+            )
             ->brandLogo(asset('images/logo-clinica.png'))
             ->brandName('Gestión de Clínica')
 	        ->brandLogoHeight('60px')
