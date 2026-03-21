5e3c875eb943cd383987ab7900b1503c8c06f474
Leandro <lean.ayala4@gmail.com>
2026-03-21 11:13:10 -0300

    Lista de estudios 2

--- app/Filament/Resources/TurnoResource.php
+++ app/Filament/Resources/TurnoResource.php
@@ -439,8 +439,21 @@ class TurnoResource extends Resource
                                                 $map = [
                                                     'radiografia' => 'Radiografía',
                                                     'analisis_sangre' => 'Análisis de sangre',
+                                                    'analisis_orina' => 'Análisis de orina',
                                                     'ecografia' => 'Ecografía',
-                                                    'resonancia' => 'Resonancia',
+                                                    'resonancia' => 'Resonancia magnética',
+                                                    'tomografia' => 'Tomografía computada',
+                                                    'electrocardiograma' => 'Electrocardiograma (ECG)',
+                                                    'ergometria' => 'Ergometría (prueba de esfuerzo)',
+                                                    'holter' => 'Holter cardíaco',
+                                                    'endoscopia' => 'Endoscopía',
+                                                    'colonoscopia' => 'Colonoscopía',
+                                                    'mamografia' => 'Mamografía',
+                                                    'densitometria_osea' => 'Densitometría ósea',
+                                                    'prueba_covid' => 'Test COVID-19',
+                                                    'perfil_lipidico' => 'Perfil lipídico',
+                                                    'glucemia' => 'Glucemia',
+                                                    'hemograma' => 'Hemograma completo',
                                                 ];
 
                                                 if (is_array($state)) {
